abe4305653a1c669ef912bd3c2bd5dd65fa9d507
Prashant Solanki <solankisrp2@gmail.com>
2016-05-07 16:39:03 +0530

    Wordlist RecyclerView Viewholder now shows Title, Privacy and Star

--- app/src/main/java/com/blackshift/verbis/ui/viewholders/WordListBottomSheetViewHolder.kt
+++ app/src/main/java/com/blackshift/verbis/ui/viewholders/WordListBottomSheetViewHolder.kt
@@ -26,7 +26,7 @@ class WordListBottomSheetViewHolder(itemView: View, context: Context) : SnapView
     }
 
     override fun populateViewHolder(wordList: WordList, i: Int) {
-        imageView.setImageDrawable(IconDrawable(context, MaterialIcons.md_collections).actionBarSize())
+        imageView.setImageDrawable(IconDrawable(context, MaterialIcons.md_format_list_bulleted).actionBarSize().colorRes(android.R.color.darker_gray))
         textView.text = wordList.title
     }
 
--- app/src/main/java/com/blackshift/verbis/ui/viewholders/WordListTitleViewHolder.kt
+++ app/src/main/java/com/blackshift/verbis/ui/viewholders/WordListTitleViewHolder.kt
@@ -2,11 +2,13 @@ package com.blackshift.verbis.ui.viewholders
 
 import android.content.Context
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
-
 import com.blackshift.verbis.R
 import com.blackshift.verbis.rest.model.wordlist.WordList
-
+import com.blackshift.verbis.utils.annotations.PrivacyLevel
+import com.joanzapata.iconify.IconDrawable
+import com.joanzapata.iconify.fonts.MaterialIcons
 import io.github.prashantsolanki3.snaplibrary.snap.adapter.AbstractSnapSelectableAdapter
 import io.github.prashantsolanki3.snaplibrary.snap.layout.viewholder.SnapSelectableViewHolder
 
@@ -23,37 +25,70 @@ import io.github.prashantsolanki3.snaplibrary.snap.layout.viewholder.SnapSelecta
 class WordListTitleViewHolder : SnapSelectableViewHolder<WordList> {
 
     internal val title: TextView
-    internal val count: TextView
+    internal val star: ImageView
+    internal val privacy: ImageView
 
     constructor(itemView: View, context: Context) : super(itemView, context) {
         title = itemView.findViewById(R.id.wordlist_title) as TextView
-        count = itemView.findViewById(R.id.wordlist_item_count) as TextView
+        star = itemView.findViewById(R.id.wordlist_title_start) as ImageView
+        privacy = itemView.findViewById(R.id.wordlist_title_privacy) as ImageView
     }
 
     constructor(itemView: View, context: Context, adapter: AbstractSnapSelectableAdapter<Any>) : super(itemView, context, adapter) {
         title = itemView.findViewById(R.id.wordlist_title) as TextView
-        count = itemView.findViewById(R.id.wordlist_item_count) as TextView
+        star = itemView.findViewById(R.id.wordlist_title_start) as ImageView
+        privacy = itemView.findViewById(R.id.wordlist_title_privacy) as ImageView
     }
 
     override fun populateViewHolder(wordList: WordList, i: Int) {
         title.text = wordList.title
-        count.text = wordList.id
+        setStarIcon(wordList.isStarred)
+        setPrivacyIcon(wordList.privacy)
+    }
+
+    fun setPrivacyIcon(privacyLevel: Int){
+        val privacyDrawable: MaterialIcons
+
+        when(privacyLevel){
+            PrivacyLevel.PUBLIC -> privacyDrawable = MaterialIcons.md_language
+
+            PrivacyLevel.PRIVATE -> privacyDrawable = MaterialIcons.md_lock_outline
+
+            PrivacyLevel.FRIENDS -> privacyDrawable = MaterialIcons.md_people_outline
+
+            else -> privacyDrawable = MaterialIcons.md_language
+        }
+
+        privacy.setImageDrawable(IconDrawable(context,privacyDrawable).colorRes(android.R.color.darker_gray).sizeDp(16))
+
+    }
+
+    fun setStarIcon(isStarred: Boolean = false){
+        val starDrawable: MaterialIcons
+        if (isStarred)
+            starDrawable = MaterialIcons.md_star
+        else
+            starDrawable = MaterialIcons.md_star_border
+
+        star.setImageDrawable(IconDrawable(context,starDrawable).colorRes(android.R.color.darker_gray).sizeDp(16))
+
     }
 
     //TODO: Complete this
     override fun onSelectionEnabled(snapSelectableViewHolder: SnapSelectableViewHolder<Any>, wordList: WordList, i: Int) {
-        itemView.setBackgroundColor(context.resources.getColor(android.R.color.darker_gray))
+        //ItemView is a CardView, use setCardBackgroundColor()
+        //itemView.setBackgroundColor(context.resources.getColor(android.R.color.darker_gray))
     }
 
     override fun onSelectionDisabled(snapSelectableViewHolder: SnapSelectableViewHolder<Any>, wordList: WordList, i: Int) {
-        itemView.setBackgroundColor(context.resources.getColor(android.R.color.white))
+        //itemView.setBackgroundColor(context.resources.getColor(android.R.color.white))
     }
 
     override fun onSelected(snapSelectableViewHolder: SnapSelectableViewHolder<Any>, wordList: WordList, i: Int) {
-        itemView.setBackgroundColor(context.resources.getColor(android.R.color.holo_blue_bright))
+        //itemView.setBackgroundColor(context.resources.getColor(android.R.color.holo_blue_bright))
     }
 
     override fun onDeselected(snapSelectableViewHolder: SnapSelectableViewHolder<Any>, wordList: WordList, i: Int) {
-        itemView.setBackgroundColor(context.resources.getColor(android.R.color.darker_gray))
+        //itemView.setBackgroundColor(context.resources.getColor(android.R.color.darker_gray))
     }
 }
